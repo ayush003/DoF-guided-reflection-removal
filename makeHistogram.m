3a0115cac5d37f7161d134e87f554dd3ec17242d
ayush003 <50289547+ayush003@users.noreply.github.com>
2019-11-24 23:52:53 +0530

    Update makeHistogram.m

--- makeHistogram.m
+++ makeHistogram.m
@@ -1,18 +1,20 @@
-function h = makeHistogram(img,nbins)   
+function h = makeHistogram(img,nbins)  
+% returns the normalised histogram of the input img
         sum = 0;
         h = zeros(1,nbins);
         [height,width] = size(img);
+        
         for yy = 1:height
             for xx = 1:width
-                v = img(yy,xx);
-                bin = uint8(v*nbins);
-                if(bin>=nbins)
+                v = img(yy,xx);   %pixel intensity
+                bin = uint8(v*nbins);  %calculating the bin for the pixel
+                if(bin>=nbins)         %sanity check for the index
                     bin = bin-1;
                 end
-                h(bin) = h(bin)+1;
-                sum = sum+1;
+                h(bin) = h(bin)+1;      %counting the number of pixels in the bin
+                sum = sum+1;            %normalisation factor = number of total pixels
             end
             
         end
-        h(find(h==0)) = 0.00000001;
-        h = h./sum;
+        h(find(h==0)) = 0.00000001; %to handle log(0/0) = NaN case
+        h = h./sum;     %normalisation
