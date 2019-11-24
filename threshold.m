efcb3e6f73dcf25254731a046155e5c07fed8bdf
ayush003 <50289547+ayush003@users.noreply.github.com>
2019-11-24 22:45:40 +0530

    Update threshold.m

--- threshold.m
+++ threshold.m
@@ -1,4 +1,4 @@
 function theta = threshold(img)
     [height,width] = size(img);
-    theta = (1/(height*width))*sum(img(:));
+    theta = (1/(height*width))*sum(img(:)); % finds the initial threshold which is the average of the intensities of the pixels
 end
