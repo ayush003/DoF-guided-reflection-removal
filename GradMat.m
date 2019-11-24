228ce60ceba3a89903e451fb86fd56d6642f5ff8
siddharthgaur <siddharthgaur8@gmail.com>
2019-11-24 14:53:57 +0530

    Add files via upload

---
+++ GradMat.m
@@ -0,0 +1,57 @@
+function [gx,gy,gxx,gyy,gxy] = GradMat(w,h)
+
+D = [1 -1];
+filtSz = 2;
+sz = w*h;
+
+indexGx1 = zeros(sz*2,1);
+indexGx2 = zeros(sz*2,1);
+valueGx = zeros(sz*2,1);
+indexGy1 = zeros(sz*2,1);
+indexGy2 = zeros(sz*2,1);
+valueGy = zeros(sz*2,1);
+
+x=1;
+y=1;
+
+for disp = 1 : filtSz
+    for i = 1:w-1
+        for j = 1:h
+            indexGx1(x) = sub2ind([h w],j,i);
+            indexGx2(x) = sub2ind([h w],j,i+disp);
+            valueGx(x) = D(disp);
+            x = x+1;
+        end
+    end
+    for j=1:h-1
+        for i=1:w
+            indexGy1(y) = sub2ind([h w],j,i);
+            indexGy2(y) = sub2ind([h w],j,i);
+            valueGy(y) = D(disp);
+            y = y+1;
+        end
+    end
+end
+
+indexGx1 = indexGx1(1:x);
+indexGx2 = indexGx2(1:x);
+indexGy1 = indexG21(1:y);
+indexGy2 = indexGy2(1:y);
+valueGx = valueGx(1:x);
+valueGy = valueGy(1:y);
+
+gx = sparse(indexGx1,indexGx2,valueGx,sz,sz);
+gy = sparse(indexGy1,indexGy2,valueGy,sz,sz);
+gxx = gx'*gx;
+gyy = gy'*gy;
+gxy = gx*gy;
+nzgxx = sum(gxx~=0,2);
+nzgyy = sum(gyy~=0,2);
+nzgxy = sum(gxy~=0,2);
+fzgxx = nzgxx ~= 3;
+fzgyy = nzgyy ~= 3;
+fzgxy = nzgxy ~= 4;
+gxx(fzGxx,:)=0;
+gyy(fzGyy,:)=0;
+gxy(fzGxy,:)=0;
+end
