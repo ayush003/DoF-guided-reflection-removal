b8568799dba1c8e7258180494ae95d0dcc950ab9
siddharthgaur <siddharthgaur8@gmail.com>
2019-11-24 16:28:27 +0530

    Driver code to run all the codes

---
+++ Driver.m
@@ -0,0 +1,99 @@
+clc;
+clear all;
+
+tic
+im = imread("153.jpg");
+
+%% Background Image %%
+lab = makecform('srgb2lab');
+Im = applycform(im,lab); %Conversion to Lab color space
+L = Im(:,:,1); a = Im(:,:,2); b = Im(:,:,3); % 3 channels L, a and b
+
+%%% Pyramid building for all channels %%%
+% Function GetPyramid returns DoFs for 3 different resolution
+% images of the same reference image of one channel
+PyramidL = GetPyramid(L);
+Pyramida = GetPyramid(a);
+Pyramida = GetPyramid(b);
+
+%%% Fusion of 3 DoFs into one DoF for one channel %%%
+% L channel %
+[N1,N2,N3] = size(PyramidL{1});
+dl1 = PyramidL{1};
+dl2 = imreasize(PyramidL{2},[N1 N2]); % upscaling
+dl3 = imreasize(PyramidL{3},[N1 N2]); % upscaling
+
+t = 0.4; % lambda = 0.4
+Lmap = (t*dl2 + (1-t)*dl3).*(dl1);
+Thl = threshold(Lmap); % thrsholding
+map1 = heaviside(Lmap - Thl); % edgemap for L color space
+
+% a channel %
+da1 = Pyramida{1};
+da2 = imreasize(Pyramida{2},[N1 N2]); % upscaling
+da3 = imreasize(Pyramida{3},[N1 N2]); % upscaling
+
+amap = (t*da2 + (1-t)*da3).*(da1);
+Tha = Thl/1.5;
+map2 = heaviside(amap - Tha); % edgemap for a color space
+
+% b channel %
+db1 = Pyramidb{1};
+db2 = imreasize(Pyramidb{2},[N1 N2]); % upscaling
+db3 = imreasize(Pyramidb{3},[N1 N2]); % upscaling
+
+bmap = (t*db2 + (1-t)*db3).*(db1);
+Thb = Thl/1.5;
+map3 = heaviside(bmap - Thb); % edgemap for b color space
+
+EdgeBackground = map1|map2|map3; % edgemap for Background
+%%
+%% Reflection image %%
+w = fspecial('sobel'); % 2D sobel filter
+for ch = 1:size(img,3)
+    im = im2double(im);
+    Gx(:,:,ch) = imfilter(im(:,:,ch),w); % gradient along x
+    Gy(:,:,ch) = imfilter(im(:,:,ch),w'); % gradient along y
+end
+grad = sqrt(Gx.^2 + Gy.^2);
+grad = max(grad,[],3); % image gradient
+reflectionPoints = find(grad<0.3&grad>0.05); % inital refection points
+mapR = zeros(N1,N2);
+mapR(reflectionPoints) = 1; % initial reflection edgemap
+
+EdgeReflection = mapR;
+
+% Masking from background edgemap and final reflection edgemap generation
+for i = 1:length(reflectionPoints)
+    pnt = reflectionPoints(i);
+    [Hp,rows,cols] = PatchExtract([N1,N2],pnt);
+    m = EdgeBackground(Hp);
+    flag = find(m==1);
+    if(EdgeBackground(pnt)==1||~isempty(flag)) 
+        EdgeReflection(pnt)= 0;
+    end
+end
+
+%%
+%% Layer Reconstruction %%
+indR = find(EdgeReflection==1);
+indB = find(EdgeBackground==1);
+
+[h,w,d] = size(grad);
+G = struct;
+[G.gx,G.gy,G.gxx,G.gyy,G.gxy] = GradMat(w,h);
+
+for ch = 1:3
+    [R(:,:,ch),B(:,:,ch)] = layerReconstruct(im(:,:,ch),indR,indB);
+end
+
+%%
+%% Output %%
+figure;
+title('Reflection Image');
+imshow(R);
+figure;
+title('Background Image');
+imshow(B);
+toc
+imshow(R);
